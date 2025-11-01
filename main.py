9b69c9c63f6ccc1069c17a86491209d9e0c0ce6e
TMCooper <pokemon160377@gmail.com>
2025-11-01 16:27:23 +0100

    Update launch function

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ except ImportError:
     from src.api import *
 
 class Api:
-    def launch():
-        Yui.app.run(debug=True)
+    @staticmethod
+    def launch(debug_state: bool = True, reload_status: bool = False):
+        Yui.app.run(debug=debug_state, use_reloader=reload_status)
 
 if __name__ == "__main__":
     Api.launch()
