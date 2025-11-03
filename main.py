2d0efb89d961f1d5c14d9b977ac2ed43bba12fde
TMCooper <pokemon160377@gmail.com>
2025-11-03 18:03:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,13 @@ except ImportError:
 
 class Api:
     @staticmethod
-    def launch(debug_state: bool = True, reload_status: bool = True):
-        Yui.app.run(debug=debug_state, use_reloader=reload_status)
+    def launch(port=5000, debug_state: bool = True, reload_status: bool = True):
+        Yui.app.run(
+            # host="0.0.0.0", # Peut être plus tard si l'api dois être accèssible a tous le reseaux
+            port=port,
+            debug=debug_state, 
+            use_reloader=reload_status
+            )
 
 if __name__ == "__main__":
     Api.launch()
