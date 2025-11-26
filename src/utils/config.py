8c71de1d565a7dfb7d5a48fe7de53b8e1a0bf089
TMCooper <91787869+TMCooper@users.noreply.github.com>
2025-11-26 03:07:57 +0100

    Update config.py

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,29 +1,41 @@
-import subprocess, shutil
+import subprocess, shutil, os
 
 class Config:
     IP = "127.0.0.1"   # valeur par défaut
     PORT = 5000        # valeur par défaut
 
 class Utils:
+
+    MODULE_ROOT = os.path.dirname(os.path.abspath(__file__))
+
+    @staticmethod
     def get_hash(ref):
-        return subprocess.check_output(['git', 'rev-parse', ref]).decode().strip()
+        return subprocess.check_output(
+            ['git', 'rev-parse', ref],
+            cwd=Utils.MODULE_ROOT
+            ).decode().strip()
 
     def hashCheck():
-        # Hash local
-        local_hash = Utils.get_hash('HEAD')
+            subprocess.run(
+                ['git', 'fetch'],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.DEVNULL,
+                cwd=Utils.MODULE_ROOT
+            )
 
-        # Récupérer les infos du remote
-        subprocess.run(['git', 'fetch'], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+            # Courante
+            branch = subprocess.check_output(
+                ['git', 'rev-parse', '--abbrev-ref', 'HEAD'],
+                cwd=Utils.MODULE_ROOT
+            ).decode().strip()
 
-        # Détecter la branche courante
-        branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode().strip()
+            # Hash local & distant
+            local_hash = Utils.get_hash('HEAD')
+            remote_hash = Utils.get_hash(f'origin/{branch}')
 
-        # Hash distant
-        remote_hash = Utils.get_hash(f'origin/{branch}')
-
-        if local_hash != remote_hash:
-            print("Please update the code : git pull origin main")
-            exit(1)
+            if local_hash != remote_hash:
+                print("Please update the code : git pull")
+                exit(1)
 
     def gitCheck():
         if shutil.which("git") is None:
